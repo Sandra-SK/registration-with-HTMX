d1e27440c5d59f24da861b89e3026e1ad428f112
Sandra K <san.kubicki@gmail.com>
2024-06-18 17:35:11 +0200

    ajout fichier 'userModel.js'

---
+++ models/userModel.js
@@ -0,0 +1,14 @@
+/////userModel.js
+
+
+import {mongoose} from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export {User};
