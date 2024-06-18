727324cf27d7909d9be69fa189142292c088089c
Sandra K <san.kubicki@gmail.com>
2024-06-18 15:27:28 +0200

    ajout type:module et transformation des requires en import

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-// server.js
-const express = require('express');
+////////  server.js
 
-const mongoose = require('mongoose');
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import {jwtMiddleware} from './middleware/auth.js';
+import {User}  from './models/userModel.js';
+dotenv.config();
 
+const __dirname = path.dirname('.');
 const app = express();
-const PORT = 3000;
-require ('dotenv').config()
 
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-app.use(express.static('public')); 
+// Middlewares
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+
 // Connect to MongoDB
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
 mongoose.connect(MONGODB_URI)
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch((err) => console.log('Connexion à MongoDB échouée !', err));
+console.log(User)
 
-// Schéma utilisateur
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-});
-const User = mongoose.model('User', userSchema);
+// Import des routes
+//import authRoutes from './routes/authRoutes.js';
+//import profileRoutes from './routes/profileRoutes.js';
 
+
+// Routes
+//app.use('/auth', authRoutes);
+//app.use(profileRoutes);
+
+
+
+// Démarrer le serveur
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
-
-//à ajouter:
-//import du middleware JWT
-//import des routes
+// Gestion des erreurs globales
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
